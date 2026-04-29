71b654a5b61af84c21e55e635995826eeadb913c
erickm13 <mejiaerick3192@gmail.com>
2026-04-29 02:44:21 -0600

    fix: install binary before share to avoid missing file error

--- Formula/salchipapa-dots.rb
+++ Formula/salchipapa-dots.rb
@@ -6,8 +6,8 @@ class SalchipapaDots < Formula
   version "v2.1.0"
 
   def install
-    (share/"salchipapa-dots").install Dir["*"]
     bin.install "install.sh" => "salchipapa-dots"
+    (share/"salchipapa-dots").install Dir["*"]
   end
 
   def caveats
